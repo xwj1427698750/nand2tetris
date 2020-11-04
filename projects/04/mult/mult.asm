7bed7f21d86fee1856e9cc76777f2123a4915201
xwj1427698750 <17352959682@163.com>
2020-11-04 22:09:01 +0800

    add mult.asm and mult.hack to projexts/04/mult

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,26 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// Note: Ri refers to the address of RAM[i],(i=0..15), namely, Ri = i
+@i
+M=1
+@R2
+M=0
+(LOOP)
+@i
+D=M   // D = i
+@R1
+D=D-M   // D = i-M[R1]
+@END
+D;JGT   //if (i-R1)>0 goto END
+@R0
+D=M     //D = M[R0]
+@R2
+M=D+M   // M[R2] = D + M[R2]
+@i
+M=M+1
+@LOOP
+0;JMP
+(END)
+@END
+0;JMP   
