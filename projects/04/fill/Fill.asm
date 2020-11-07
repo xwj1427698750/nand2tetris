c1642bca5300725a47e8489b33462502d75aff8f
xwj1427698750 <17352959682@163.com>
2020-11-07 15:23:06 +0800

    add fill files to projects/04/fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,55 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP)
+@SCREEN
+D=A
+@i
+M=D   //M[i] = SCREEN
+@j
+M=D   //M[j] = SCREEN
+
+@KBD
+D=M
+@CLEARLOOP
+D;JEQ      //M[KBD]==0 跳转到CLEARLOOP 操作
+
+(FILLLOOP) //填充操作
+@i
+D=M        //D = M[i]
+@KBD
+D=D-A      //D = M[i] - KBD
+@FILLEND
+D;JGE      //if(M[i] - KBD) >=0 : go to FILLEND
+@i
+A=M        //A = M[i]
+M=-1       //M[A]=-1, 填充屏幕
+@i
+M=M+1      //M[i]=M[i]+1
+@FILLLOOP
+0;JMP
+(FILLEND)
+@LOOP
+0;JMP
+
+(CLEARLOOP) //清除操作
+@j
+D=M        //D = M[j]
+@KBD
+D=D-A      //D = M[j] - KBD
+@CLEAREND
+D;JGE      //if(M[j] - KBD) >=0 : go to CLEAREND
+@j
+A=M        //A = M[j]
+M=0        //M[A]=0, 清除屏幕
+@j
+M=M+1      //M[j]=M[j]+1
+@CLEARLOOP
+0;JMP
+(CLEAREND)
+@LOOP
+0;JMP
+
+
+
+
